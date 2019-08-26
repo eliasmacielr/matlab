6139683fa9bcfaf37414873e846ed09574a61bc9
Elias Maciel <eliasmacielr@gmail.com>
2019-08-26 19:43:49 -0400

    Implement ORDER 1, PMX and MOX

--- EvolutionaryAlgorithms/I3_TSP/iteretic_algorithm.m
+++ EvolutionaryAlgorithms/I3_TSP/iteretic_algorithm.m
@@ -31,24 +31,65 @@ new_pop(p/2+1:3*p/4, :)=winners;
 % -- Crossover
 crossover=randperm(p/2);
 children=zeros(p/4, n);
+% When using MOX
+children2=zeros(p/4, n);
 for i=2:2:p/2
-    parent1=winners_r1(crossover(i-1), :);
+    % Original code
+%     parent1=winners_r1(crossover(i-1), :);
+%     parent2=winners_r1(crossover(i), :);
+%     child=parent2;
+%     ndx=ceil(n*sort(rand(1, 2)));
+%     while ndx(1)==ndx(2)
+%         ndx=ceil(n*sort(rand(1, 2)));
+%     end
+%     tmp=parent1(ndx(1):ndx(2));
+%     for j=1:length(tmp)
+%         child(child==tmp(j))=0;
+%     end
+%     %child=[child(1:ndx(1)) tmp child(ndx(1)+1:n)];
+%     % ORDER 1
+%     child=[child(1:n) tmp];
+%     child=nonzeros(child)';
+%     children(i/2, :)=child;
+    %
+    % PMX
+%     parent1=winners_r1(crossover(i-1), :);
+%     parent2=1:n;
+%     child=zeros(1,n);
+%     ndx=ceil(n/2*sort(rand(1, 2)));
+%     ndx(2)=ndx(1)+n/2;
+%     child(ndx(1):ndx(2))=parent1(ndx(1):ndx(2));
+%     tmp=setdiff(parent2(ndx(1):ndx(2)),parent1(ndx(1):ndx(2)));
+%     % try to fit tmp right to the left of the copied part from parent1
+%     if length(tmp) > ndx(1)-1 % tmp does not fit at the beginning
+%         child(1:ndx(1)-1)=tmp(1:ndx(1)-1);
+%         tmp_2=tmp(ndx(1):end);
+%         child(ndx(2)+1:ndx(2)+length(tmp_2))=tmp_2;
+%         child(ndx(2)+length(tmp_2)+1:n)= ...
+%             parent2(ndx(2)+length(tmp_2)+1:n);
+%     else % tmp fits to the left of the copied part
+%         idx=ndx(1)-length(tmp);
+%         child(idx:ndx(1)-1)=tmp;
+%         child(1:idx-1)=parent2(1:idx-1);
+%         child(ndx(2)+1:n)=parent2(ndx(2)+1:n);
+%     end
+%     children(i/2, :)=child;
+    %
+    % MOX
+    parent1=1:n;
     parent2=winners_r1(crossover(i), :);
-    child=parent2;
-    ndx=ceil(n*sort(rand(1, 2)));
-    while ndx(1)==ndx(2)
-        ndx=ceil(n*sort(rand(1, 2)));
-    end
-    tmp=parent1(ndx(1):ndx(2));
-    for j=1:length(tmp)
-        child(child==tmp(j))=0;
-    end
-    child=[child(1:ndx(1)) tmp child(ndx(1)+1:n)];
-    child=nonzeros(child)';
-    children(i/2, :)=child;
+    ndx=ceil(n*sort(rand(1)));
+    child1=[parent1(1:ndx-1) intersect(parent1(ndx:n), parent2, 'stable')];
+    child2=[parent2(1:ndx-1) intersect(parent2(ndx:n), parent1, 'stable')];
+    children(i/2, :)=child1;
+    children2(i/2, :)=child2;
+    %
 end
+% new_pop(p/4+1:p/2, :)=children;
+% new_pop(3*p/4+1:p, :)=children;
+% When using MOX (comment the two lines above)
 new_pop(p/4+1:p/2, :)=children;
-new_pop(3*p/4+1:p, :)=children;
+new_pop(3*p/4+1:p, :)=children2;
 % -- Mutate
 mutate=randperm(p/2);
 num_mutate=round(mutate_rate*p/2);
