1e800d842068c1a4481e0914cd21dbf5e2d08519
Elias Maciel <eliasmacielr@gmail.com>
2019-08-26 17:37:30 -0400

    Implement TWORS, RSM and PSM
    RSM implementation was already in the book.

--- EvolutionaryAlgorithms/I3_TSP/iteretic_algorithm.m
+++ EvolutionaryAlgorithms/I3_TSP/iteretic_algorithm.m
@@ -57,6 +57,21 @@ for i=1:num_mutate
     while ndx(1)==ndx(2)
         ndx=ceil(n*sort(rand(1, 2)));
     end
+    % TWORS
+%     if mod(ndx(2)-ndx(1)+1, 2) ~= 0
+%         ndx(2)=ndx(2)-1;
+%     end
+%     subarray=new_pop(p/2+mutate(i), ndx(1):ndx(2));
+%     idxs=randperm(length(subarray));
+%     for j=1:((ndx(2)-ndx(1)+1)/2)
+%         subarray([idxs(j) idxs(j*2)])=subarray([idxs(j*2) idxs(j)]);
+%     end
+%     new_pop(p/2+mutate(i), ndx(1):ndx(2))=subarray;
+    % RSM
     new_pop(p/2+mutate(i), ndx(1):ndx(2))= ...
         fliplr(new_pop(p/2+mutate(i), ndx(1):ndx(2)));
+    % PSM
+%     subarray=new_pop(p/2+mutate(i), ndx(1):ndx(2));
+%     new_pop(p/2+mutate(i), ndx(1):ndx(2))= ...
+%         subarray(randperm(length(subarray)));
 end
