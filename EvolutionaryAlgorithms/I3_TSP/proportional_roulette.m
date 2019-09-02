1ba5fd1e9d57d125cb61cef0702420995f01f265
Elias Maciel <eliasmacielr@gmail.com>
2019-09-02 13:39:25 -0400

    Revert to previous implementation using a vector of rand numbers

--- EvolutionaryAlgorithms/I3_TSP/proportional_roulette.m
+++ EvolutionaryAlgorithms/I3_TSP/proportional_roulette.m
@@ -8,16 +8,10 @@ fitness_roulette=n./fitness; % new fitness in order to select the shortest paths
 fitness_roulette=fitness_roulette.*n; % "normalize" fitness_roulette value
 winners=zeros(p/2, n);
 fitness_roulette_cumperc=cumsum(fitness_roulette./sum(fitness_roulette));
-selected=zeros(1,p/2);
-i = 1;
-while length(nonzeros(selected)) < 50
-    idx=find(fitness_roulette_cumperc>=rand);
-    if ismember(idx(1), selected)
-        continue
-    end
-    selected(i)=idx(1);
+roulette=rand(1,p/2);
+for i=1:length(roulette)
+    idx=find(fitness_roulette_cumperc>=roulette(i));
     winners(i, :)=pop(idx(1), :);
-    i=i+1;
 end
 new_pop(1:p/4, :)=winners(1:p/4, :);
 new_pop(p/2+1:3*p/4, :)=winners(p/4+1:p/2, :);
