14f098c02810ec7b432f0354b5abfe4e5d5a0250
Elias Maciel <eliasmacielr@gmail.com>
2019-09-02 13:19:04 -0400

    proportional_roulette.m: select different individuals (50%)

--- EvolutionaryAlgorithms/I3_TSP/proportional_roulette.m
+++ EvolutionaryAlgorithms/I3_TSP/proportional_roulette.m
@@ -1,29 +1,23 @@
 % -- Proportional Roulette
-function [new_pop, winners_r1]=proportional_roulette(pop, fitness)
+function [new_pop, winners]=proportional_roulette(pop, fitness)
 % -- Roulette Selection - Round One
 [p, n]=size(pop);
 new_pop=zeros(p, n);
 format long
-fitness=1.0./fitness; % new fitness in order to select the shortest paths
-fitness=fitness.*100000; % "normalize" fitness value
-winners_r1=zeros(p/2, n);
-tmp_fitness=zeros(1, p/2);
-fitness_cumperc=cumsum(fitness./sum(fitness));
-roulette=rand(1,p/2);
-for i=1:length(roulette)
-    idx=find(fitness_cumperc>=roulette(i));
-    idx=idx(1);
-    winners_r1(i, :)=pop(idx, :);
-    tmp_fitness(i)=fitness(idx);
+fitness_roulette=n./fitness; % new fitness in order to select the shortest paths
+fitness_roulette=fitness_roulette.*n; % "normalize" fitness_roulette value
+winners=zeros(p/2, n);
+fitness_roulette_cumperc=cumsum(fitness_roulette./sum(fitness_roulette));
+selected=zeros(1,p/2);
+i = 1;
+while length(nonzeros(selected)) < 50
+    idx=find(fitness_roulette_cumperc>=rand);
+    if ismember(idx(1), selected)
+        continue
+    end
+    selected(i)=idx(1);
+    winners(i, :)=pop(idx(1), :);
+    i=i+1;
 end
-% -- Roulette Selection - Round Two
-winners=zeros(p/4, n);
-fitness_cumperc=cumsum(tmp_fitness./sum(tmp_fitness));
-roulette=rand(1,p/4);
-for i=1:length(roulette)
-    idx=find(fitness_cumperc>=roulette(i));
-    idx=idx(1);
-    winners(i, :)=winners_r1(idx, :);
-end
-new_pop(1:p/4, :)=winners;
-new_pop(p/2+1:3*p/4, :)=winners;
+new_pop(1:p/4, :)=winners(1:p/4, :);
+new_pop(p/2+1:3*p/4, :)=winners(p/4+1:p/2, :);
