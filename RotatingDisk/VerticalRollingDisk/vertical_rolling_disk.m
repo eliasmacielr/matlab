9e0136e693cbef1545863918536a58d2350cf6f8
Elias Maciel <eliasmacielr@gmail.com>
2019-11-10 15:54:38 -0300

    Draw static disk

--- RotatingDisk/VerticalRollingDisk/vertical_rolling_disk.m
+++ RotatingDisk/VerticalRollingDisk/vertical_rolling_disk.m
@@ -2,24 +2,32 @@ clear all %#ok<CLALL>
 close all
 clc
 
-T = 10; % Time of the simulation (s)
+T = 5; % Time of the simulation (s)
 dt = 0.01; % Time step (s)
 N = T/dt;
 
-% TODO: comment all these
-R = 0.5; % Radius of the disk (m)
-x = zeros(N,1);
-y = zeros(N,1);
-z = zeros(N,1);
-phi = zeros(N,1);
-theta = zeros(N,1);
+x = zeros(N, 1);
+y = zeros(N, 1);
+z = zeros(N, 1);
+phi = zeros(N, 1);
+theta = zeros(N, 1);
+
+% Disk
+R = 0.5; % Radius (m)
+sides = 48;
+angle = linspace(0, 2*pi, sides)';
+circ1 = R*cos(angle);
+circ2 = R*sin(angle) + R;
+xDisk = zeros(sides,N);
+yDisk = zeros(sides,N);
+zDisk = zeros(sides,N);
 
 % Initial conditions
 x0 = 0;
 y0 = 0;
 phi0 = pi/2;
 theta0 = 0;
-omega = pi/9;
+omega = pi/4;
 Omega = 4*sqrt(2);
 phi(1) = phi0;
 for t = 1:N
@@ -27,6 +35,10 @@ for t = 1:N
     y(t) = -(Omega/omega)*R*cos(omega*t*dt + phi0) + y0;
     phi(t) = omega*t*dt + phi0;
     theta(t) = Omega*t*dt + theta0;
+    % TODO: apply rotation matrices
+    xDisk(:,t) = x(t) + zeros(sides, 1);
+    yDisk(:,t) = y(t) + circ1;
+    zDisk(:,t) = z(t) + circ2;
 end
 
 % Setup video
@@ -43,8 +55,7 @@ zlim([0, 1.2*(2*R)])
 grid on
 
 path = line('xdata', x(1:1), 'ydata', y(1:1), 'zdata', z(1:1), 'color', 'b', 'linewidth', 2);
-% disk = patch('xdata', xcirc(:,1), 'ydata', ycirc(:,1), 'zdata', zcirc(:,1), 'facecolor', [1, 0.8, 0.6], 'linewidth', 2);
-% pointA = line('xdata', xA(1), 'ydata', yA(1), 'zdata', zA(1), 'marker', 'o', 'color', 'r', 'markerfacecolor', 'r', 'linewidth', 3);
+disk = patch('xdata', xDisk(:,1), 'ydata', yDisk(:,1), 'zdata', zDisk(:,1), 'facecolor', [1, 0.8, 0.6], 'linewidth', 2);
 
 animation = VideoWriter('vertical-rolling-disk.avi');
 animation.FrameRate = 1/dt;
@@ -52,8 +63,7 @@ open(animation);
 
 for t = 1:N
     set(path, 'xdata', x(1:t), 'ydata', y(1:t), 'zdata', z(1:t));
-%     set(disk, 'xdata', xcirc(:,t), 'ydata', ycirc(:,t), 'zdata', zcirc(:,t));
-%     set(pointA, 'xdata', xA(t), 'ydata', yA(t), 'zdata', zA(t));
+    set(disk, 'xdata', xDisk(:,t), 'ydata', yDisk(:,t), 'zdata', zDisk(:,t));
     drawnow
     writeVideo(animation, getframe(gcf));
 end 
