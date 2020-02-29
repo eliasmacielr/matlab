d587b6e79ccc0313546e5396eb469e2512c88f0c
Elias Maciel <eliasmacielr@gmail.com>
2020-02-29 11:17:28 -0300

    Remove work done in Evolutionary Algorithms

--- EvolutionaryAlgorithms/I1_MAX/gaDemo1Eval.m
+++
@@ -1,3 +0,0 @@
-function [sol, val]=gaDemo1Eval(sol,~)
-x=sol(1);
-val=x+10*sin(5*x)+7*cos(4*x);
--- EvolutionaryAlgorithms/I1_MAX/mutate.m
+++
@@ -1,3 +0,0 @@
-function result=mutate(genotype)
-% Possibly mutates a genotype
-result=abs(genotype - rand(size(genotype,1), size(genotype,2))<0.03);
