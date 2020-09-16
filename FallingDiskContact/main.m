7bef94b9ffe6191bdb1ab7e3643266c098a9ac88
Elias Maciel <eliasmacielr@gmail.com>
2020-09-16 16:00:13 -0400

    Rewrite system of equations using the discrete Lagrange

--- FallingDiskContact/main.m
+++ FallingDiskContact/main.m
@@ -5,91 +5,120 @@ close all
 clc
 
 %% Symbols
-% q_{k-1}
+% q_{j-1}
+syms X_i Y_i theta_i phi_i psi_i z_i
+% q_j
 syms X_j Y_j theta_j phi_j psi_j z_j
-% q_k
+% q_{j+1}
 syms X_k Y_k theta_k phi_k psi_k z_k
-% q_{k+1}
-syms X_l Y_l theta_l phi_l psi_l z_l
 % constants
-m = 1;
 R = 0.15;
-I_A = 2;
+m = 1;
+I_A = 1;
 I_T = 1;
 g = 9.8;
-alpha = 0.1;
+alpha = 1;
 % "Control" forces
 u_psi = 0;
 % Lagrange multipliers
 syms lambda_1 lambda_2
 % step size
-h = 0.1;
+h = 0.2;
 
-%% Disk discrete equations
-% TODO: fix the system, there's something wrong, or the integrator does not
-% work
-lambda_1 = m*(-X_j + 2*X_k - X_l)/h^2 - m*alpha*(-X_j + X_k)/h;
-lambda_2 = m*(-Y_j + 2*Y_k - Y_l)/h^2 - m*alpha*(-Y_j + Y_k)/h;
-eq_theta = m*(R^2*(sin(theta_k)*cos(theta_k)*((theta_l-theta_k)/h)^2 ...
-    - (sin(theta_k^2))^2*(theta_l-theta_k)/h^2 + ...
-    (sin(theta_j))^2*(theta_k-theta_j)/h^2)) + ...
-    I_A*((psi_l-psi_k)/h - ...
-        (phi_l-phi_k)/h*sin(theta_k))*(-(phi_l-phi_k)/h*cos(theta_k)) + ...
-    I_T*((theta_k-theta_l)/h^2 - ...
-        ((phi_l-phi_k)/h)^2*cos(theta_k)*sin(theta_k) + ...
-        (theta_k-theta_j)/h^2) + m*g*R*sin(theta_k) - ...
-    lambda_1*R*cos(theta_k)*sin(phi_k) + ...
-    lambda_2*R*cos(theta_k)*cos(phi_k);
-eq_phi = I_A*(((psi_l-psi_k)/h - ...
-    (phi_l-phi_k)/h*sin(theta_k))*sin(theta_k)/h - ...
-    ((psi_k-psi_j)/h - (phi_k-phi_j)/h*sin(theta_j))*sin(theta_j)/h) + ...
-    I_T*((cos(theta_k))^2*(phi_l-phi_k)/h^2 + (cos(theta_j))^2/h) - ...
-    lambda_1*R*sin(theta_k)*cos(phi_k) - ...
-    lambda_2*R*sin(theta_k)*sin(phi_k);
-eq_psi = I_A*((-psi_l+psi_k)/h^2 + (phi_l-phi_k)/h*sin(theta_k) + ...
-    (psi_k-psi_j)/h^2 - (phi_k-phi_j)/h^2*sin(theta_j)) + u_psi + ...
-    lambda_1*R*cos(phi_k) + lambda_2*R*sin(phi_k);
+%% Discrete Lagrangians and nonholonomic constraints
+% Write L(q_j,q_{j+1},z_j,z_{j+1})
+% q
+X = X_j;
+Y = Y_j;
+theta = theta_j;
+phi = phi_j;
+psi = psi_j;
+% q_dot
+X_dot = (X_k-X_j)/h;
+Y_dot = (Y_k-Y_j)/h;
+theta_dot = (theta_k-theta_j)/h;
+phi_dot = (phi_k-phi_j)/h;
+psi_dot = (psi_k-psi_j)/h;
 
-% Constraints
-eq_X = ((X_l-X_k)/h) + R*cos(theta_k)*sin(phi_k)*(theta_l-theta_k)/h + ...
-    R*sin(theta_k)*cos(phi_k)*(phi_l-phi_k)/h - ...
-    R*cos(phi_k)*(psi_l-psi_k)/h;
-eq_Y = ((Y_l-X_k)/h) - R*cos(theta_k)*cos(phi_k)*(theta_l-theta_k)/h + ...
-    R*sin(theta_k)*sin(phi_k)*(phi_l-phi_k)/h - ...
-    R*sin(phi_k)*(psi_l-psi_k)/h;
+L_j = 1/2*m*(X_dot^2 + Y_dot^2 + R^2*(sin(theta))^2*theta_dot^2) + ...
+    1/2*(I_A*(psi_dot - phi_dot*sin(theta))^2 + ...
+         I_T*(theta_dot^2 + phi_dot^2*(cos(theta))^2)) - ...
+    m*g*R*(cos(theta_j)+cos(theta_k))/2 - alpha*z_j + ...
+    (u_psi*psi_j+u_psi*psi_k)/2;
+
+% Write constraints (uses q_j and q_{j+1})
+Omega_d_1 = X_dot + R*cos(theta)*sin(phi)*theta_dot + ...
+    R*sin(theta)*cos(phi)*phi_dot - R*cos(phi)*psi_dot;
+Omega_d_2 = Y_dot - R*cos(theta)*cos(phi)*theta_dot + ...
+    R*sin(theta)*sin(phi)*phi_dot - R*sin(phi)*psi_dot;
+
+% Write L(q_{j-1},q_j,z_{j-1},z_j)
+% q
+X = X_i;
+Y = Y_i;
+theta = theta_i;
+phi = phi_i;
+psi = psi_i;
+% q_dot
+X_dot = (X_j-X_i)/h;
+Y_dot = (Y_j-Y_i)/h;
+theta_dot = (theta_j-theta_i)/h;
+phi_dot = (phi_j-phi_i)/h;
+psi_dot = (psi_j-psi_i)/h;
+
+L_i = 1/2*m*(X_dot^2 + Y_dot^2 + R^2*(sin(theta))^2*theta_dot^2) + ...
+    1/2*(I_A*(psi_dot - phi_dot*sin(theta))^2 + ...
+         I_T*(theta_dot^2 + phi_dot^2*(cos(theta))^2)) - ...
+    m*g*R*(cos(theta_i)+cos(theta_j))/2 - alpha*z_i + ...
+    (u_psi*psi_i+u_psi*psi_j)/2;
+
+% Precompute the term that is multiplied to D_2 L(q_{j-1},q_j,...)
+partial_z = (1 + h*diff(L_j,z_j))/(1 - h*diff(L_i,z_j));
+
+lambda_1 = diff(L_j,X_j) + diff(L_i,X_j)*partial_z;
+lambda_2 = diff(L_j,Y_j) + diff(L_i,Y_j)*partial_z;
+
+eq_theta = diff(L_j,theta_j) + diff(L_i,theta_j)*partial_z - ...
+    lambda_1*R*cos(theta_j)*sin(phi_j) + ...
+    lambda_2*R*cos(theta_j)*cos(phi_j);
+eq_phi = diff(L_j,phi_j) + diff(L_i,phi_j)*partial_z - ...
+    lambda_1*R*sin(theta_j)*cos(phi_j) - ...
+    lambda_2*R*sin(theta_j)*sin(phi_j);
+eq_psi = diff(L_j,psi_j) + diff(L_i,psi_j)*partial_z + ...
+    lambda_1*R*cos(phi_j) + lambda_2*R*sin(phi_j);
 
 %% Initial conditions and Integration
-T = 1;
-h = 0.1;
+T = 5;
 N = int32(T/h);
 
 q = zeros(5, N);
 
+% q(:,j) = (X(j),Y(j),theta(j),phi(j),psi(j))
 q(:,1) = [0; 0; 0; 0; 0];
 q(:,2) = [R*pi/12; 0; 0; 0; pi/12];
 
-for k = 2:N-1
-    X_j     = q(1,k-1);
-    Y_j     = q(2,k-1);
-    theta_j = q(3,k-1);
-    phi_j   = q(4,k-1);
-    psi_j   = q(5,k-1);
+for j = 2:N-1
+    X_i     = q(1,j-1);
+    Y_i     = q(2,j-1);
+    theta_i = q(3,j-1);
+    phi_i   = q(4,j-1);
+    psi_i   = q(5,j-1);
 
-    X_k     = q(1,k);
-    Y_k     = q(2,k);
-    theta_k = q(3,k);
-    phi_k   = q(4,k);
-    psi_k   = q(5,k);
+    X_j     = q(1,j);
+    Y_j     = q(2,j);
+    theta_j = q(3,j);
+    phi_j   = q(4,j);
+    psi_j   = q(5,j);
 
-    sol_k = newton_n_dim(1e-4, q(:,k)', ...
-        [X_l Y_l theta_l phi_l psi_l], ...
-        [subs(eq_theta) subs(eq_phi) subs(eq_psi) subs(eq_X) ...
-        subs(eq_Y)]', 1000);
-    q(:,k+1) = sol_k';
+    sol_j = newton_n_dim(1e-3, q(:,j)', ...
+        [X_k Y_k theta_k phi_k psi_k], ...
+        [subs(eq_theta) subs(eq_phi) subs(eq_psi) ...
+        subs(Omega_d_1) subs(Omega_d_2)]', 100);
+    q(:,j+1) = sol_j';
 end
 
 %% Animation
 animate_rolling_disk(q(1,:),q(2,:),q(3,:),q(4,:),q(5,:),R,h);
 
 %% Save last simulation results
-% TODO: save q
+save('vars.mat','q','R','h');
