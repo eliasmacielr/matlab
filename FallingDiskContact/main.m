6fd798f14108c5a8df86b1be578a80c796458bb9
Elias Maciel <eliasmacielr@gmail.com>
2020-09-18 12:37:51 -0400

    Use simplify function for system of equations
    * Plot the system's position and velocity.

--- FallingDiskContact/main.m
+++ FallingDiskContact/main.m
@@ -17,7 +17,7 @@ m = 1;
 I_A = 1;
 I_T = 1;
 g = 9.8;
-alpha = 1;
+alpha = 0.1;
 % "Control" forces
 u_psi = 0;
 % Lagrange multipliers
@@ -48,9 +48,9 @@ L_j = 1/2*m*(X_dot^2 + Y_dot^2 + R^2*(sin(theta))^2*theta_dot^2) + ...
 
 % Write constraints (uses q_j and q_{j+1})
 Omega_d_1 = X_dot + R*cos(theta)*sin(phi)*theta_dot + ...
-    R*sin(theta)*cos(phi)*phi_dot - R*cos(phi)*psi_dot;
+    R*sin(theta)*cos(phi)*phi_dot - R*cos(phi)*psi_dot; % (1)
 Omega_d_2 = Y_dot - R*cos(theta)*cos(phi)*theta_dot + ...
-    R*sin(theta)*sin(phi)*phi_dot - R*sin(phi)*psi_dot;
+    R*sin(theta)*sin(phi)*phi_dot - R*sin(phi)*psi_dot; % (2)
 
 % Write L(q_{j-1},q_j,z_{j-1},z_j)
 % q
@@ -75,6 +75,10 @@ L_i = 1/2*m*(X_dot^2 + Y_dot^2 + R^2*(sin(theta))^2*theta_dot^2) + ...
 % Precompute the term that is multiplied to D_2 L(q_{j-1},q_j,...)
 partial_z = (1 + h*diff(L_j,z_j))/(1 - h*diff(L_i,z_j));
 
+% Write system of equations in the form F(q) = 0, which with the
+% constraint equations (1) and (2) make up the whole system to be
+% solved.
+
 lambda_1 = diff(L_j,X_j) + diff(L_i,X_j)*partial_z;
 lambda_2 = diff(L_j,Y_j) + diff(L_i,Y_j)*partial_z;
 
@@ -88,14 +92,14 @@ eq_psi = diff(L_j,psi_j) + diff(L_i,psi_j)*partial_z + ...
     lambda_1*R*cos(phi_j) + lambda_2*R*sin(phi_j);
 
 %% Initial conditions and Integration
-T = 5;
+T = 2;
 N = int32(T/h);
 
 q = zeros(5, N);
 
 % q(:,j) = (X(j),Y(j),theta(j),phi(j),psi(j))
-q(:,1) = [0; 0; 0; 0; 0];
-q(:,2) = [R*pi/12; 0; 0; 0; pi/12];
+q(:,1) = [0; 0; pi/6; 0; 0];
+q(:,2) = [R*pi/12; 0; pi/(6*(1+h)); 0; pi/12];
 
 for j = 2:N-1
     X_i     = q(1,j-1);
@@ -110,15 +114,25 @@ for j = 2:N-1
     phi_j   = q(4,j);
     psi_j   = q(5,j);
 
-    sol_j = newton_n_dim(1e-3, q(:,j)', ...
-        [X_k Y_k theta_k phi_k psi_k], ...
-        [subs(eq_theta) subs(eq_phi) subs(eq_psi) ...
-        subs(Omega_d_1) subs(Omega_d_2)]', 100);
+    F = simplify([subs(eq_theta) subs(eq_phi) subs(eq_psi) ...
+        subs(Omega_d_1) subs(Omega_d_2)]');
+
+    sol_j = newton_n_dim(1e-5, q(:,j)', ...
+        [X_k Y_k theta_k phi_k psi_k], F, 100);
     q(:,j+1) = sol_j';
 end
 
-%% Animation
+%% Animation and state space portraits
 animate_rolling_disk(q(1,:),q(2,:),q(3,:),q(4,:),q(5,:),R,h);
+
+t = linspace(0,T,N);
+plot(t,q(1,:),t,q(2,:),t,q(3,:),t,q(4,:),t,q(5,:))
+legend({'X','Y','\theta','\phi','\psi'},'Interpreter','tex')
+t = linspace(0,T,N-1);
+plot(t,diff(q(1,:))/h,t,diff(q(2,:))/h,t,diff(q(3,:))/h, ...
+    t,diff(q(4,:))/h,t,diff(q(5,:))/h);
+legend({'X^{''}','Y^{''}','\theta^{''}','\phi^{''}','\psi^{''}'}, ...
+    'Interpreter','tex')
 
 %% Save last simulation results
 save('vars.mat','q','R','h');
