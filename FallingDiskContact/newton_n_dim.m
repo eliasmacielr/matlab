b98ebcc610981f95ddfc78b5c140c97a54ed4d7c
Elias Maciel <eliasmacielr@gmail.com>
2020-09-28 22:46:49 -0400

    Refactor function
    * Delete code to solve undetermined system.
    
    * Change variable names.
    
    * Use break keyword instead of stop variable.

--- FallingDiskContact/newton_n_dim.m
+++ FallingDiskContact/newton_n_dim.m
@@ -1,14 +1,12 @@
-function [X] = newton_n_dim(tolerance_rss,initial_estimate,sym_variables,sym_equations,maxit)
+function [x,i] = newton_n_dim(tol,x0,sym_vars,sym_eqns,maxit)
 %% newton method for solving a system of >=n nonlinear equations for n variables
 % Given n equations, the function performs the newton method, converging
 % to the exact solution.
-% Given >n equations, the function converges to the solution which minimizes
-% the least squared error of the given equations.
 %
-%input:     tolerance_norm :        maximum tolerable RSS of errors in output vector
-%           initial estimate :      row vector of initial estimate
-%           sym_variables:          row vector of n symbolic variables
-%           sym_equations:          column vector of >=n symbolic equations
+%input:     tol:        maximum tolerable RSS of errors in output vector
+%           x0:         row vector of initial estimate
+%           sym_vars:   row vector of n symbolic variables
+%           sym_eqns:   column vector of >=n symbolic equations
 %
 %output:    solution:               row vector of solution.
 %
@@ -24,39 +22,24 @@ function [X] = newton_n_dim(tolerance_rss,initial_estimate,sym_variables,sym_equ
 %        initial_est = [10,1];
 %with n equations and n unknowns:
 %        solution = newton_n_dim(tolerance,initial_est,[a,b],[F1;F2]);
-%with >n equations and n unknowns:
-%        F3 = sqrt(a^2 + b^2)-15.5; (third equation, (15.5 = sqrt(a^2 + b^2)))
-%        solution = newton_n_dim(tolerance,initial_est,[a,b],[F1;F2;F3]);
 %Kyle J. Drerup
 %Ohio University EECS
 %11-9-2010
 %%  the code...
-H = jacobian(sym_equations,sym_variables);
-X = initial_estimate;
-n_equations = 0;
-if length(sym_equations)==length(sym_variables)
-    n_equations = 1;
-end
-print_n_iterations = 0;
-stop = 0;
+J = jacobian(sym_eqns,sym_vars);
+x = x0;
+
 i = 1;
-while ~stop
-    F_X = subs(sym_equations,sym_variables,X);
-    F_prime_X = subs(H,sym_variables,X);
-    if ~isnumeric(F_prime_X)
-        F_prime_X = eval(F_prime_X);
+while 1
+    F = subs(sym_eqns,sym_vars,x);
+    F_prime = subs(J,sym_vars,x);
+    if ~isnumeric(F_prime)
+        F_prime = eval(F_prime);
     end
-    if n_equations == 1
-        d_X = F_prime_X \ F_X;
-    else % overdetermined solution, use generalized inverse matrix
-        d_X = ((F_prime_X.'*F_prime_X)^-1)*F_prime_X.'*F_X;
-    end
-    X = X - d_X.';
-    if sqrt(sum(d_X.^2)) <= tolerance_rss
-        stop = 1;
-        if print_n_iterations
-            fprintf("%d, %.4f\n", i, sqrt(sum(d_X.^2)))
-        end
+    d_x = F_prime \ F;
+    x = x - d_x.';
+    if sqrt(sum(d_x.^2)) <= tol
+        break
     end
     if i >= maxit
         error("Maximum number of iterations exceeded.")
