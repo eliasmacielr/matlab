5f2c929a137afc42993c500c8b466856a3f66bbd
Elias Maciel <eliasmacielr@gmail.com>
2020-09-29 22:44:46 -0400

    main: add forces and refactor code
    * Use variables t0 and tf to set the time of simulation.
    
    * Use tj for the forces functions.
    
    * Do not call subs for each equation, call this function once per
    iteration for all the equations put in a column vector.

--- FallingDiskContact/main.m
+++ FallingDiskContact/main.m
@@ -18,8 +18,8 @@ syms R m I_A I_T g alpha
 u_psi = 0;
 % Lagrange multipliers
 syms lambda_1 lambda_2
-% step size
-syms h;
+% time and step size
+syms tj h
 
 assume((R > 0) & (m > 0) & (I_A > 0) & (I_T > 0) & (g > 0) & ...
     (alpha > 0) & (h > 0))
@@ -36,11 +36,16 @@ theta_dot = (theta_k-theta_j)/h;
 phi_dot = (phi_k-phi_j)/h;
 psi_dot = (psi_k-psi_j)/h;
 
+f_j = [0 0 0*tj 0*tj 0*tj];
+f_k = [0 0 0*(tj+h) 0*(tj+h) 0*(tj+h)];
+q_j = [X_j Y_j theta_j phi_j psi_j]';
+q_k = [X_k Y_k theta_k phi_k psi_k]';
+
 L_j = 1/2*m*(X_dot^2 + Y_dot^2 + R^2*(sin(theta))^2*theta_dot^2) + ...
     1/2*(I_A*(psi_dot - phi_dot*sin(theta))^2 + ...
          I_T*(theta_dot^2 + phi_dot^2*(cos(theta))^2)) - ...
     m*g*R*(cos(theta_j)+cos(theta_k))/2 - alpha*z_j + ...
-    (u_psi*psi_j+u_psi*psi_k)/2;
+    (f_j*q_j+f_k*q_k)/2;
 
 % Write constraints (uses q_j and q_{j+1})
 Omega_d_1 = X_dot + R*cos(theta)*sin(phi)*theta_dot + ...
@@ -62,11 +67,16 @@ theta_dot = (theta_j-theta_i)/h;
 phi_dot = (phi_j-phi_i)/h;
 psi_dot = (psi_j-psi_i)/h;
 
+f_i = [0 0 0*(tj-h) 0*(tj-h) 0*(tj-h)];
+f_j = [0 0 0*tj 0*tj 0*tj];
+q_i = [X_i Y_i theta_i phi_i psi_i]';
+q_j = [X_j Y_j theta_j phi_j psi_j]';
+
 L_i = 1/2*m*(X_dot^2 + Y_dot^2 + R^2*(sin(theta))^2*theta_dot^2) + ...
     1/2*(I_A*(psi_dot - phi_dot*sin(theta))^2 + ...
          I_T*(theta_dot^2 + phi_dot^2*(cos(theta))^2)) - ...
     m*g*R*(cos(theta_i)+cos(theta_j))/2 - alpha*z_i + ...
-    (u_psi*psi_i+u_psi*psi_j)/2;
+    (f_i*q_i+f_j*q_j)/2;
 
 % Precompute the term that is multiplied to D_2 L(q_{j-1},q_j,...)
 partial_z = (1 + h*diff(L_j,z_j))/(1 - h*diff(L_i,z_j));
@@ -95,9 +105,12 @@ I_T = 1;
 g = 9.8;
 alpha = 0.1;
 
-T = 3;
+t0 = 0;
+tf = 3;
+T = tf - t0;
 h = 0.2;
 N = int32(T/h);
+tol = 1e-4;
 
 eq_theta = subs(eq_theta);
 eq_phi = subs(eq_phi);
@@ -108,8 +121,9 @@ Omega_d_2 = subs(Omega_d_2);
 q = zeros(5, N);
 
 % q(:,j) = [X(j);Y(j);theta(j);phi(j);psi(j)]
-q(:,1) = [0; 0; pi/6; 0; 0];
-q(:,2) = [R*pi/12; 0; pi/6; 0; pi/12];
+q(:,1) = [0; 0; 0; 0; 0];
+% TODO: obtener q(:,2) a partir de una aproximacion
+q(:,2) = [R*pi/12; 0; 0; 0; pi/12];
 
 for j = 2:N-1
     X_i     = q(1,j-1);
@@ -124,34 +138,35 @@ for j = 2:N-1
     phi_j   = q(4,j);
     psi_j   = q(5,j);
 
-    [q(:,j+1), i] = newton_n_dim(1e-3, q(:,j), ...
-        [X_k; Y_k; theta_k; phi_k; psi_k], ...
-        [subs(eq_theta); subs(eq_phi); subs(eq_psi); ...
-        subs(Omega_d_1); subs(Omega_d_2)], 10);
-    fprintf("%.5f\n", i);
+    tj = t0 + j*h;
+
+    q(:,j+1) = newton_n_dim(q(:,j), [X_k; Y_k; theta_k; phi_k; psi_k], ...
+        subs([eq_theta; eq_phi; eq_psi; Omega_d_1; Omega_d_2]), tol, 10);
 end
 
 %% Animation and state space portraits
 animate_rolling_disk(q(1,:),q(2,:),q(3,:),q(4,:),q(5,:),R,h);
 
-% figure
-% 
-% subplot(2,1,1);
-% t = linspace(0,T,N);
-% plot(t,q(1,:),t,q(2,:),t,q(3,:),t,q(4,:),t,q(5,:))
-% legend({'X','Y','\theta','\phi','\psi'},'Interpreter','tex')
-% xlabel('Tiempo (s)')
-% title('Posición del sistema, q(0) = (0,0,\pi/6,0,0)','Interpreter','tex')
-% 
-% subplot(2,1,2);
-% % Todo, ver como sacar las velocidades
-% t = linspace(0,T,N-1);
-% plot(t,diff(q(1,:))/h,t,diff(q(2,:))/h,t,diff(q(3,:))/h, ...
-%     t,diff(q(4,:))/h,t,diff(q(5,:))/h);
-% legend({'X^{''}','Y^{''}','\theta^{''}','\phi^{''}','\psi^{''}'}, ...
-%     'Interpreter','tex')
-% xlabel('Tiempo (s)')
-% title('Velocidad del sistema, q''(0) = (0,0,\pi/6,0,0)','Interpreter','tex')
-% 
-% %% Save last simulation results
-% save('vars.mat','q','R','h');
+figure
+
+subplot(2,1,1);
+t = linspace(0,T,N);
+plot(t,q(1,:),t,q(2,:),t,q(3,:),t,q(4,:),t,q(5,:))
+legend({'X','Y','\theta','\phi','\psi'},'Interpreter','tex')
+xlabel('Tiempo (s)')
+title('Posición del sistema, q(0) = (0,0,\pi/6,0,0)','Interpreter','tex')
+
+subplot(2,1,2);
+% Todo, ver como sacar las velocidades
+t = linspace(0,T,N-1);
+plot(t,diff(q(1,:))/h,t,diff(q(2,:))/h,t,diff(q(3,:))/h, ...
+    t,diff(q(4,:))/h,t,diff(q(5,:))/h);
+legend({'X^{''}','Y^{''}','\theta^{''}','\phi^{''}','\psi^{''}'}, ...
+    'Interpreter','tex')
+xlabel('Tiempo (s)')
+title('Velocidad del sistema, q''(0) = (0,0,\pi/6,0,0)','Interpreter','tex')
+
+savefig(strcat('resultados-',num2str(tol),'-',num2str(T),'s'));
+
+%% Save last simulation results
+save(strcat('vars-',num2str(tol),'-',num2str(T),'s.mat'),'q','R','h');
