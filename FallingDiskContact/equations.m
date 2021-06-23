8f3563bca10ac5cc26c81e03b283cdab754bc026
Elias Maciel <eliasmacielr@gmail.com>
2021-06-22 23:43:45 -0400

    main.m: declare forces
    Declare forces in this file and use symbolic forces in equations.m.

--- FallingDiskContact/equations.m
+++ FallingDiskContact/equations.m
@@ -10,12 +10,21 @@ syms X_i Y_i theta_i phi_i psi_i z_i
 syms X_j Y_j theta_j phi_j psi_j z_j
 % q_{j+1}
 syms X_k Y_k theta_k phi_k psi_k z_k
-% constants
-syms R m I_A I_T g alpha
+% Dissipation
+syms alpha
+% External forces
+% F_{j-1}
+syms F_X_i F_Y_i F_theta_i F_phi_i F_psi_i
+% F_j
+syms F_X_j F_Y_j F_theta_j F_phi_j F_psi_j
+% F_{j+1}
+syms F_X_k F_Y_k F_theta_k F_phi_k F_psi_k
+% Constants
+syms R m I_A I_T g
 % Lagrange multipliers
 syms lambda_1 lambda_2
-% time and step size
-syms tj h
+% step size
+syms h
 
 assume((R > 0) & (m > 0) & (I_A > 0) & (I_T > 0) & (g > 0) & ...
     (alpha > 0) & (h > 0))
@@ -32,8 +41,8 @@ theta_dot = (theta_k-theta_j)/h;
 phi_dot = (phi_k-phi_j)/h;
 psi_dot = (psi_k-psi_j)/h;
 
-f_j = [0 0 0*tj 0*tj 1/2];
-f_k = [0 0 0*(tj+h) 0*(tj+h) 1/2];
+F_j = [F_X_j F_Y_j F_theta_j F_phi_j F_psi_j];
+F_k = [F_X_k F_Y_k F_theta_k F_phi_k F_psi_k];
 q_j = [X_j; Y_j; theta_j; phi_j; psi_j];
 q_k = [X_k; Y_k; theta_k; phi_k; psi_k];
 
@@ -41,7 +50,7 @@ L_j = 1/2*m*(X_dot^2 + Y_dot^2 + R^2*sin(theta)^2*theta_dot^2) + ...
     1/2*(I_A*(psi_dot - phi_dot*sin(theta))^2 + ...
          I_T*(theta_dot^2 + phi_dot^2*cos(theta)^2)) - ...
     m*g*R*(cos(theta_j)+cos(theta_k))/2 - alpha*(z_j+z_k)/2 + ...
-    (f_j*q_j+f_k*q_k)/2;
+    (F_j*q_j+F_k*q_k)/2;
 
 % Write constraints (uses q_j and q_{j+1})
 Omega_d_1 = X_dot + R*cos(theta)*sin(phi)*theta_dot + ...
@@ -63,14 +72,14 @@ theta_dot = (theta_j-theta_i)/h;
 phi_dot = (phi_j-phi_i)/h;
 psi_dot = (psi_j-psi_i)/h;
 
-f_i = [0 0 0*(tj-h) 0*(tj-h) 1/2];
+F_i = [F_X_i F_Y_i F_theta_i F_phi_i F_psi_i];
 q_i = [X_i; Y_i; theta_i; phi_i; psi_i];
 
 L_i = 1/2*m*(X_dot^2 + Y_dot^2 + R^2*sin(theta)^2*theta_dot^2) + ...
     1/2*(I_A*(psi_dot - phi_dot*sin(theta))^2 + ...
          I_T*(theta_dot^2 + phi_dot^2*cos(theta)^2)) - ...
     m*g*R*(cos(theta_i)+cos(theta_j))/2 - alpha*(z_i+z_j)/2 + ...
-    (f_i*q_i+f_j*q_j)/2;
+    (F_i*q_i+F_j*q_j)/2;
 
 % Precompute the term that is multiplied to D_2 L(q_{j-1},q_j,...)
 partial_z = (1 + h*diff(L_j,z_j))/(1 - h*diff(L_i,z_j));
--- FallingDiskContact/main.m
+++ FallingDiskContact/main.m
@@ -7,9 +7,8 @@ R = 0.5;
 m = 5;
 I_A = 1/2*m*R^2;
 I_T = 1/4*m*R^2;
-g = 9.81;
 
-alpha = 0; % dissipation parameter
+g = 9.81;
 
 t0 = 0;
 tf = 50;
@@ -20,7 +19,7 @@ tol = 1e-6;
 
 span = [.8 1.2];
 
-F = vpa(subs([eq_theta; eq_phi; eq_psi; Omega_d_1; Omega_d_2]));
+S = vpa(subs([eq_theta; eq_phi; eq_psi; Omega_d_1; Omega_d_2]));
 
 % Initial conditions
 % theta0 = 20*(pi/180);
@@ -28,6 +27,10 @@ F = vpa(subs([eq_theta; eq_phi; eq_psi; Omega_d_1; Omega_d_2]));
 % psidot0 = ((I_T-I_A-m*R^2)*sin(theta0)*phidot0^2-m*g*R)/((I_A+m*R^2)*tan(theta0)*phidot0);
 q0 = [0; 0; 0; 0; 0];
 qdot0 = [0; 0; 0; 0; 0];
+% Dissipation parameter
+alpha = 0;
+% External forces
+F = {@(t) 0, @(t) 0, @(t) 0, @(t) 0, @(t) 1/2};
 
 q = zeros(5, N);
 q(:,1) = vpa(q0);
@@ -52,9 +55,23 @@ for j = 2:N-1
     phi_j   = q(4,j);
     psi_j   = q(5,j);
 
-    [q(:,j+1), i] = newton_n_dim(q(:,j), q_k, subs(F), tol, 10);
+    F_X_i     = F{1}(tj-h);
+    F_Y_i     = F{2}(tj-h);
+    F_theta_i = F{3}(tj-h);
+    F_phi_i   = F{4}(tj-h);
+    F_psi_i   = F{5}(tj-h);
+
+    F_X_j     = F{1}(tj);
+    F_Y_j     = F{2}(tj);
+    F_theta_j = F{3}(tj);
+    F_phi_j   = F{4}(tj);
+    F_psi_j   = F{5}(tj);
+
+    [q(:,j+1), i] = newton_n_dim(q(:,j), q_k, subs(S), tol, 10);
 %     fprintf("%d\n", i);
 end
+
+t = t0:h:tf;
 
 % Get coordinates
 X = q(1,:);
@@ -76,8 +93,8 @@ E = 1/2*m*(Xdot.^2 + Ydot.^2 + R^2*sin(theta).*thetadot.^2) + ...
     m*g*R*cos(theta);
 
 %% Get reference solution
-% TODO: change to h/10
-[t,y] = ode15i(@diskODEs,t0:h:tf,y0,yp0,odeset('RelTol',tol));
+h_ref = h / 10;
+[t_ref,y] = ode15i(@diskODEs,t0:h_ref:tf,y0,yp0,odeset('RelTol',tol));
 
 % Get coordinates
 X_ref = y(:,1);
@@ -87,11 +104,11 @@ phi_ref = y(:,4);
 psi_ref = y(:,5);
 
 % Compute velocities from coordinates
-Xdot_ref = [y0(6);diff(X_ref)/h];
-Ydot_ref = [y0(7);diff(Y_ref)/h];
-thetadot_ref = [y0(8);diff(theta_ref)/h];
-phidot_ref = [y0(9);diff(phi_ref)/h];
-psidot_ref = [y0(10);diff(psi_ref)/h];
+Xdot_ref = [y0(6);diff(X_ref)/h_ref];
+Ydot_ref = [y0(7);diff(Y_ref)/h_ref];
+thetadot_ref = [y0(8);diff(theta_ref)/h_ref];
+phidot_ref = [y0(9);diff(phi_ref)/h_ref];
+psidot_ref = [y0(10);diff(psi_ref)/h_ref];
 
 E_ref = 1/2*m*(Xdot_ref.^2 + Ydot_ref.^2 + R^2*sin(theta_ref).*thetadot_ref.^2) + ...
     1/2*(I_A*(psidot_ref - phidot_ref.*sin(theta_ref)).^2 + ...
@@ -106,11 +123,20 @@ E_ref = 1/2*m*(Xdot_ref.^2 + Ydot_ref.^2 + R^2*sin(theta_ref).*thetadot_ref.^2) 
 figure
 plot(t,X,'--x','MarkerIndices',1:10:length(X))
 hold on
-plot(t,X_ref)
+plot(t_ref,X_ref)
 xlim([t0 tf])
 legend('Contacto (orden 2)','Referencia')
 xlabel('Tiempo (s)')
 ylabel('X (m)')
+
+figure
+plot(t,E,'--x','MarkerIndices',1:10:length(X))
+hold on
+plot(t_ref,E_ref)
+xlim([t0 tf])
+legend('Contacto (orden 2)','Referencia')
+xlabel('Tiempo (s)')
+ylabel('Energía (J)')
 
 % figure
 % subplot(2,1,1)
